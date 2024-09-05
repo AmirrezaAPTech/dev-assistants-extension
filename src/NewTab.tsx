63f1bf57951ab25f4f5b68ca41d755dee544f05e
AmirrezaAP <AmirrezaAPTech@gmail.com>
2024-09-05 12:45:57 +0330

    Fix: Newtab Padding

--- src/NewTab.tsx
+++ src/NewTab.tsx
@@ -13,8 +13,8 @@ const NewTab = () => {
   const [bookmarkFullVisible, setBookmarkFullVisible] = useState(false)
 
   return (
-    <div className={`min-h-screen h-full flex justify-center xl:items-center items-start max-xl:py-12 w-screen min-w-[580px] overflow-auto NewTab`}>
-      <div className="xl:grid xl:grid-cols-4 xl:h-[80vh] h-full min-h-[10rem] gap-x-3 xl:w-[1200px] max-xl:max-w-[700px] w-full max-xl:flex max-xl:flex-col-reverse max-xl:justify-center max-xl:gap-y-6 max-xl:items-center">
+    <div className={`min-h-screen h-full flex justify-center items-start py-12 w-screen min-w-[580px] overflow-auto NewTab`}>
+      <div className="xl:grid xl:grid-cols-4 h-full min-h-[10rem] gap-x-3 xl:w-[1200px] max-xl:max-w-[700px] w-full max-xl:flex max-xl:flex-col-reverse max-xl:justify-center max-xl:gap-y-6 max-xl:items-center">
           <div className="w-full rounded-2xl bg-white gap-y-2 relative p-2 shadow-2xl xl:hidden">
             <CalenderWidget />
           </div>
